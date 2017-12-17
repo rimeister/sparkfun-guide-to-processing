c592952f43c2e4b2382883e8de621831b1516e92
Riley Boyd <contact@rileyboyd.com>
2017-12-17 12:03:13 -0500

    Project 5: Created a Matrix with three red circles in it

---
+++ project_5/build-tmp/source/project_5.java
@@ -0,0 +1,38 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class project_5 extends PApplet {
+
+public void setup() {
+	
+}
+
+public void draw() {
+	pushMatrix();
+	fill(255,0,0);
+	ellipse(0,0,25,25);
+	ellipse(0,25,25,25);
+	ellipse(0,50,25,25);
+	popMatrix();
+}
+  public void settings() { 	size(250,250); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "project_5" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
